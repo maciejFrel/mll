116447ba3fb0d167eea8250485a53c21a0978db3
mvy <mvy@mvy>
2023-11-13 22:30:54 +0100

    a

--- 51/a.py
+++ 51/a.py
@@ -2,6 +2,8 @@ from pathlib import Path
 import pandas as pd
 import tarfile
 import urllib.request
+import matplotlib.pyplot as plt
+import numpy as np
 
 def load_data():
     path = Path("datasets/housing.tgz")
@@ -13,4 +15,24 @@ def load_data():
             housing.extractall(path="datasets")
     return pd.read_csv(Path("datasets/housing/housing.csv"))
 
+def shuffle_and_split_data(data, test_ratio):
+    np.random.seed(42)
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
 housing = load_data()
+
+# print(housing.head())
+# housing.info()
+# print(housing["ocean_proximity"].value_counts())
+# print(housing.describe())
+
+# housing.hist(bins=50, figsize=(12, 8))
+# plt.show()
+
+train_set, test_set = shuffle_and_split_data(housing, 0.2)
+
+# print(train_set.tail(1))
